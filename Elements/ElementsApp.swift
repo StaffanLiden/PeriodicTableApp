e17a9760a8655863bfa13a8b6c5e348016eb8938
Staffan Liden <staffan.liden@gmail.com>
2025-03-05 20:04:30 +0100

    Initial Commit

---
+++ Elements/ElementsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElementsApp.swift
+//  Elements
+//
+//  Created by Staffan Liden on 2025-03-05.
+//
+
+import SwiftUI
+
+@main
+struct ElementsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
